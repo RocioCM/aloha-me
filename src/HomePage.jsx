dd66d3e11504267966709d25833aaa6121d155b0
RoM <rociocmena@gmail.com>
2020-11-05 03:11:06 -0300

    Filters working

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -17,30 +17,112 @@ class HomePage extends React.Component {
 		};
 	}
 
-	//Not in use.
-	handleCountryChange() {
-		let hotels = this.props.hotels;
-		if (!country) hotels = this.state.selectedHotels;
-		let filteredHotels = hotels.filter(
-			(hotel) => hotel.country === e.target.value
-		);
-		this.setState({selectedHotels: hotels});
-	}
-
-	//Not in use.
-	handleFilters = (event) => {};
-
 	handleFilter = (e) => {
+		//Triggered when any of the filter value updates.
+		//This updates the state and triggers the cards filter function.
+
 		this.setState(
 			{[e.target.name]: e.target.value !== '' ? e.target.value : null},
 			() => {
-				console.log(this.state); ///Should I here launch specific filters-state-change functions? */
+				const filteredHotels = this.runFilters(this.props.hotels);
+				this.setState({selectedHotels: filteredHotels});
 			}
 		);
 	};
 
+	runFilters = (hotels) => {
+		//Runs the five different filters on the full hotels list.
+
+		let filteredHotels = this.filterByDate(
+			hotels,
+			'availabilityFrom',
+			this.state.arrivalDate
+		);
+		filteredHotels = this.filterByDate(
+			filteredHotels,
+			'availabilityTo',
+			this.state.leaveDate
+		);
+		filteredHotels = this.filterByPropValue(
+			filteredHotels,
+			'country',
+			this.state.country
+		);
+		filteredHotels = this.filterByPropValue(
+			filteredHotels,
+			'price',
+			this.state.price
+		);
+		filteredHotels = this.filterBySize(filteredHotels, this.state.size);
+		return filteredHotels;
+	};
+
+	filterByDate(hotels, property, value) {
+		//Filters hotels list by date of arrival or leave.
+		//'property´ param specifies if it's arrival or leave date.
+		//'value' param is the ISO string of the selected date.
+
+		if (!value) {
+			return hotels;
+		}
+
+		const getDateObj = (formattedDate) => {
+			const dateParams = formattedDate.split('-');
+			return new Date(dateParams[0], dateParams[1] - 1, dateParams[2]);
+		};
+
+		const date = getDateObj(value).valueOf();
+
+		switch (property) {
+			case 'availabilityFrom':
+				return hotels.filter(
+					(hotel) =>
+						hotel[property] <= date &&
+						hotel['availabilityTo'] > date
+				);
+			case 'availabilityTo':
+				return hotels.filter(
+					(hotel) =>
+						hotel[property] >= date &&
+						hotel['availabilityFrom'] < date
+				);
+			default:
+				return hotels;
+		}
+	}
+
+	filterBySize(hotels, size) {
+		//Filters hotels list by the number of rooms it has.
+
+		switch (size) {
+			case 'pequeño':
+				return hotels.filter((hotel) => hotel.rooms <= 10);
+			case 'mediano':
+				return hotels.filter(
+					(hotel) => hotel.rooms > 10 && hotel.rooms <= 20
+				);
+			case 'grande':
+				return hotels.filter((hotel) => hotel.rooms > 20);
+			default:
+				return hotels;
+		}
+	}
+
+	filterByPropValue(hotels, property, value) {
+		//Filters hotels list by a given property.
+		//Outputs a list of the objects which given 'property' has the given 'value'.
+
+		if (!value) {
+			//If value is null, it doesn´t filter.
+			return hotels;
+		}
+		return hotels.filter((hotel) => hotel[property] == value);
+	}
+
 	getCountryOptions(hotels) {
-		//Gets the list of all countries to choose. Outputs an Array of Formatted Objects.
+		//Gets the list of all countries to choose.
+		//Outputs an Array of Formatted-Country-Option-Objects.
+
 		let countries = [];
 		hotels.forEach((hotel) => {
 			if (!countries.includes(hotel.country)) {
